9b557b9182fe13289bdbd61d82d051d84c3b692a
Oleksiy Kost <AlekseyKo2208@gmail.com>
2026-03-26 06:07:09 +0200

    add dod-mod-1-1

--- js/mod-1/task-mod.js
+++ js/mod-1/task-mod.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log("Hello world");
+// console.log("Hello world");
 
 // 1)
 
@@ -8,9 +8,9 @@ function makeTransaction(quantity, pricePerDroid) {
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
 
 // 2)
 
@@ -19,9 +19,9 @@ function getShippingMessage(country, price, deliveryFee) {
   return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
 
 // 3)
 
@@ -32,6 +32,6 @@ function getElementWidth(content, padding, border) {
   return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
