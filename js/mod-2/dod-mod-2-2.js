8a4f6b17c86fc54daa13daf6391b73e01a4397ab
Oleksiy Kost <AlekseyKo2208@gmail.com>
2026-04-15 22:30:53 +0300

    task

--- js/mod-2/dod-mod-2-2.js
+++ js/mod-2/dod-mod-2-2.js
@@ -19,6 +19,14 @@
 
 // 1. Напиши цикл, який знайде та виведе на консоль суму всіх чисел від 1 до 100.
 
+// const order = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+//   23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+//   42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+// ];
+
+// console.table(order);
+
 // function calculateTotalPrice(order) {
 //   let sumOrder = 0;
 //   for (const element of order) {
@@ -26,6 +34,8 @@
 //   }
 //   return sumOrder;
 // }
+
+// console.log(calculateTotalPrice(order));
 
 // let sum = 0;
 // for (let i = 1; i <= 100; i += 1) {
@@ -36,18 +46,89 @@
 
 // 1. Напиши цикл, який виведе на консоль таблицю множення для числа 7 (від 1 до
 //    10).
+
+// for (let i = 1; i <= 10; i += 1) {
+//   const table = 7 * i;
+//   const res = `7 * ${i} = ${table}`;
+//   console.table(res);
+// }
+
 // 1. Напиши цикл, який обчислить факторіал числа, введеного користувачем
 //    (наприклад, факторіал числа 5: `!5 = 1 * 2 * 3 * 4 * 5`).
 
+// const factorial = +prompt("Enter number");
+// let res = 1;
+// for (let i = 1; i <= factorial; i += 1) {
+//   res = res * i;
+// }
+// console.log(res);
+
 // 1. Напиши цикл, який виведе кожну другу літеру рядочку "Hello world"
+
+// const str = "Hello world";
+// let everyTwoLetter = "";
+// for (let i = 1; i < str.length; i += 2) {
+//   everyTwoLetter += str[i];
+// }
+// console.log(everyTwoLetter);
+
 // 1. Напиши цикл, який виведе кожну літеру рядочку "Hello world" у зворотньому
 //    порядку.
+
+// const str = "Hello world";
+// let strReverse = "p";
+// for (let i = str[str.length - 1]; i >= str[0]; i -= 1) {
+//   console.log(i);
+
+//   strReverse += [i];
+// }
+// console.log(strReverse);
+
 // 1. Напиши цикл, який виведе кожну літеру рядочку "Hello world" поки не зустріне
 //    пробіл.
+
+// const str = "Hello world";
+
+// for (let i = 0; i <= str.length - 1; i += 1) {
+//   if (str[i] === " ") {
+//     break;
+//   }
+//   let newStr = str[i];
+//   console.log(newStr);
+// }
+
 // 1. Напиши цикл, який виведе на консоль кожну другу літеру слова "JavaScript".
 
+// const str = "JavaScript";
+
+// for (let i = 0; i <= str.length - 1; i += 2) {
+//   const newStr = str[i];
+//   console.log(newStr);
+// }
+
 // 1. Напиши функцію, яка приймає рядок та повертає перевернутий рядок
+
+// function strReverse(str) {
+//   const strReverse = str.split(" ").reverse().join(" ");
+//   return strReverse;
+// }
+
+// const res1 = strReverse("TRY Java");
+// console.log(res1);
+
 // 1. Напиши функцію, яка приймає рядок та повертає рядок де кожен другий символ у
 //    верхньому регістрі
+
+function strUpper(str) {
+  let newStr = "";
+  for (let i = 0; i <= str.length - 1; i += 1) {
+    newStr += str[i].toUpperCase();
+    return strUpper;
+  }
+}
+
+const res1 = strUpper("java script");
+console.log(res1);
+
 // 1. Напиши функцію getString(str1, str2, count). Функція повинна додати str2 до
 //    рядочка str1 стільки разів скільки вказали у параметрі count.
